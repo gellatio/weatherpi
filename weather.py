d02ae29a9c0be6b2428e5f2bed235400f592e0a9
softgrass <26261205+softgrass@users.noreply.github.com>
2021-05-30 17:43:37 -0400

    Removed logging
    Removed since InitialState is a paid service now

--- weather.py
+++ weather.py
@@ -2,10 +2,10 @@
 from sense_hat import SenseHat
 import time
 import sys
-from ISStreamer.Streamer import Streamer
+# from ISStreamer.Streamer import Streamer
 
 sense = SenseHat()
-logger = Streamer(bucket_name="Sense Hat Sensor Data", access_key="API KEY HERE!")
+# logger = Streamer(bucket_name="Sense Hat Sensor Data", access_key="API KEY HERE!")
 sense.clear()
 
 try:
@@ -13,15 +13,18 @@ try:
 
 		temp = sense.get_temperature()
 		temp = 1.8 * round(temp, 1) + 32
-		logger.log("Temperature F :",temp)
+		sense.show_message("Temperature: " + str(temp) + "F")
+		# logger.log("Temperature F :",temp)
 		
 		humidity = sense.get_humidity()
 		humidity= round(humidity, 1)
-		logger.log("Humidity :",humidity)
+		sense.show_message("Humidity: " + str(humidity))
+		# logger.log("Humidity :",humidity)
                 
 		pressure = sense.get_pressure()
                 pressure = round(pressure, 1)
-                logger.log("Pressure :",pressure)
+		sense.show_message("Pressure : " + str(pressure))
+                # logger.log("Pressure :",pressure)
 		
 		time.sleep(1)
 
