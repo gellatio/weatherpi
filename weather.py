06d35ef93676ccc9489fdef907ad5dbe945bf702
softgrass <26261205+softgrass@users.noreply.github.com>
2021-05-30 17:51:54 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -11,26 +11,29 @@ sense.clear()
 
 try:
 	while True:
-
+		now = datetime.now()
+		dt = now.strftime("%d/%m/%Y %H:%M:%S")
 		temp = sense.get_temperature()
-		temp = 1.8 * round(temp, 1) + 32
-		sense.show_message("Temperature: " + str(temp) + "F")
-		# logger.log("Temperature F :",temp)
-		
 		humidity = sense.get_humidity()
-		humidity= round(humidity, 1)
-		sense.show_message("Humidity: " + str(humidity))
-		# logger.log("Humidity :",humidity)
-                
 		pressure = sense.get_pressure()
-                pressure = round(pressure, 1)
-		sense.show_message("Pressure : " + str(pressure))
-                # logger.log("Pressure :",pressure)
 		
-		print("*** " + datetime.now() + " ***")
+		temp = 1.8 * round(temp, 1) + 32
+		humidity= round(humidity, 1)
+		pressure = round(pressure, 1)
+		
+		
+		print("*** " + dt + " ***")
 		print("Temperature: " + str(temp) + "F")
 		print("Humidity: " + str(humidity))
 		print("Pressure : " + str(pressure))
+		
+		sense.show_message("Temperature: " + str(temp) + "F")
+		# logger.log("Temperature F :",temp)
+		sense.show_message("Humidity: " + str(humidity))
+		# logger.log("Humidity :",humidity)
+		sense.show_message("Pressure : " + str(pressure))
+                # logger.log("Pressure :",pressure)
+		
 		time.sleep(1)
 
 except KeyboardInterrupt:
